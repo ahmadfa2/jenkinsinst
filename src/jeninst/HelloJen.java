1b66bcbf9a3e206fd7c20ca51219da1040c3fed2
Farooq Ahmad <farooq.ahmad@fortum.com>
2018-11-09 13:12:16 +0100

    modified:   src/jeninst/HelloJen.java

--- src/jeninst/HelloJen.java
+++ src/jeninst/HelloJen.java
@@ -4,7 +4,7 @@ public class HelloJen {
 
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println("hello world " + i);
+			System.out.println("hello world a" + i);
 
 		}
 	}
